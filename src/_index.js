2dad793433a5093de456233e68d7bb59a2d57416
meglenchev <t.meglenchev@gmail.com>
2025-09-16 17:00:05 +0300

    optimization of the index.js and backup old index.js

--- src/_index.js
+++ src/_index.js
@@ -9,23 +9,48 @@ const server = http.createServer( async (req, res) => {
 
     if (req.url === '/') {
         const homePage = await getHome();
+
+        // res.writeHead(200, {
+        //     'content-type': 'text/html'
+        // });
+
+        //res.write(homePage);
         view = homePage;
     } else if (req.url === '/cats/add-breed') {
         const addBread = await getAddBreed();
+
+        // res.writeHead(200, {
+        //     'content-type': 'text/html'
+        // })
+
+        //res.write(addBread);
         view = addBread;
     } else if (req.url === '/cats/add-cat') {
         const addCat = await getAddCat();
+
+        // res.writeHead(200, {
+        //     'content-type': 'text/html'
+        // });
+
+        //res.write(addCat);
         view = addCat;
     } else if (req.url === '/styles/site.css') {
         const siteCss = await getStyles();
+
         contentType = 'text/css';
+
+        // res.writeHead(200, {
+        //     'content-type': 'text/css'
+        // });
+
+        //res.write(siteCss);
         view = siteCss
     }
 
     res.writeHead(200, {
         'content-type': contentType
     });
-
+    
     res.write(view);
 
     res.end();
